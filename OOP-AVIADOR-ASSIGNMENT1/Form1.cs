ff484a2ae7922a9252c839ae5248050d4cc5e990
Nieltrbl <danielaviador24@gmail.com>
2022-04-28 19:04:14 +0800

    I have added a new feature called OOP-AVIADOR-ASSIGNMENT1

---
+++ OOP-AVIADOR-ASSIGNMENT1/Form1.cs
@@ -0,0 +1,16 @@
+
+namespace OOP_AVIADOR_ASSIGNMENT1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Daniel Diestro Aviador", "Hello my name iss", MessageBoxButtons.OKCancel);
+        }
+    }
+}
